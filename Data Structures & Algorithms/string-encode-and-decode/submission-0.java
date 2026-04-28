1bace3ffba23514abe3d0e317842cf9797048880
SACHITA LIMAYE <sachita.limaye23@gmail.com>
2026-04-28 23:01:58 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb=new StringBuilder();
+        for(String s:strs){
+            sb.append(s.length());
+            sb.append("#");
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> list=new ArrayList<>();
+        int i=0;
+        while(i<str.length()){
+            int j=i;
+            while(str.charAt(j)!='#'){
+                j++;
+            }
+            int l=Integer.parseInt(str.substring(i,j));
+            String s=str.substring(j+1,j+1+l);
+            list.add(s);
+            i=j+l+1;
+        }
+        return list;
+    }
+}
